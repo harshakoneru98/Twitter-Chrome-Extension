4a8b0bc7f89a458a293a33781f76b71e0a46fccc
Harsha Vardhan Koneru <hkoneru@usc.edu>
2023-01-07 16:36:26 +0700

    Test Deployed Web Server

--- server/app.js
+++ server/app.js
@@ -44,6 +44,10 @@ app.use((req, res, next) => {
 // Routes which should handle requests
 app.use('/api', twitterRoutes);
 
+app.get('/', function (req, res) {
+    res.send('Server Working as expected');
+});
+
 app.use((req, res, next) => {
     const error = new Error('Not found');
     error.status = 404;
