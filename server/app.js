e265bb2420e509f2fae55730f53507c63afbac53
Harsha Vardhan Koneru <hkoneru@usc.edu>
2023-01-09 17:12:26 +0700

    CORS resolution

--- server/app.js
+++ server/app.js
@@ -28,6 +28,7 @@ app.use(bodyParser.json());
 // Handling CORS Errors
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', true);
     res.header(
         'Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type, Accept, Authorization'
