1f4c7a2ba99e0128d1567140018dfd4577da8e73
Harsha Vardhan Koneru <hkoneru@usc.edu>
2023-01-11 06:02:08 +0700

    Updated scripts for performance

--- contentScript.js
+++ contentScript.js
@@ -19,6 +19,36 @@ let waitForElm = (selector) => {
     });
 };
 
+let extract_content_xpath = (selector, type) => {
+    let results = document.evaluate(
+        selector,
+        document,
+        null,
+        XPathResult.ANY_TYPE,
+        null
+    );
+
+    let output = [];
+
+    if (type == 'tweet_text') {
+        while ((node = results.iterateNext())) {
+            let tweet_text = node.innerHTML
+                .replace(/<img .*?alt="([^"]+)"[^>]*>/g, ' $1 ') // extract alt text from img tag
+                .replace(/<\/?[a-z][^>]*>/g, ' ') // remove all tags
+                .replace(/\s+/g, ' ')
+                .trim(); // cleanup whitespace
+            output.push(tweet_text);
+        }
+    } else {
+        while ((node = results.iterateNext())) {
+            let user_tweet_text = node.lastChild.children[0];
+            output.push(user_tweet_text);
+        }
+    }
+
+    return output;
+};
+
 // Detects if the tweet is in english langauage or not
 let language_detection_endpoint = async (tweets) => {
     let body = tweets.map((tweet) => ({
@@ -66,161 +96,121 @@ let sentiment_score_endpoint = async (tweets) => {
     }
 };
 
-waitForElm('section').then(async (elm) => {
-    // Initialize tweets storage
-    await chrome.storage.sync.remove('tweets');
-    await chrome.storage.sync.remove('user_tweets');
-    await chrome.storage.sync.set({ tweets: [] });
-    await chrome.storage.sync.set({ user_tweets: [] });
+let mood_emoji = (mood) => {
+    if (mood == 'POSITIVE') {
+        return String.fromCodePoint(128522);
+    } else if (mood == 'NEUTRAL') {
+        return String.fromCodePoint(128528);
+    } else if (mood == 'NEGATIVE') {
+        return String.fromCodePoint(128577);
+    } else {
+        return 'NO_MOOD';
+    }
+};
 
-    // Select the node that will be observed for mutations
-    const targetNode = document.getElementsByTagName('section')[0];
+let generate_moods = (indexes, moods) => {
+    output = [];
+    mood_index = 0;
+    for (let i = 0; i < indexes.length; i++) {
+        if (indexes[i] == 1) {
+            output.push(mood_emoji(moods[mood_index]));
+            mood_index += 1;
+        } else {
+            output.push('NO_MOOD');
+        }
+    }
 
-    // Options for the observer (which mutations to observe)
-    const config = { childList: true, subtree: true };
+    return output;
+};
 
-    // Callback function to execute when mutations are observed
-    const callback = async (mutationList, observer) => {
-        for (const mutation of mutationList) {
-            if (mutation.type === 'childList') {
-                let results = document.evaluate(
-                    '//article[@data-testid="tweet"]//div[@data-testid="tweetText"]',
-                    document,
-                    null,
-                    XPathResult.ANY_TYPE,
-                    null
-                );
+let scrollStop = (callback, refresh = 66) => {
+    // Make sure a valid callback was provided
+    if (!callback || typeof callback !== 'function') return;
 
-                let user_results = document.evaluate(
-                    '//article[@data-testid="tweet"]//div[@data-testid="User-Names"]',
-                    document,
-                    null,
-                    XPathResult.ANY_TYPE,
-                    null
-                );
+    // Setup scrolling variable
+    let isScrolling;
 
-                let tweets = [];
-                let user_tweets = [];
+    // Listen for scroll events
+    window.addEventListener(
+        'scroll',
+        function (event) {
+            // Clear our timeout throughout the scroll
+            window.clearTimeout(isScrolling);
 
-                // Extract tweet text even with icons
-                while ((node = results.iterateNext())) {
-                    let tweet_text = node.innerHTML
-                        .replace(/<img .*?alt="([^"]+)"[^>]*>/g, ' $1 ') // extract alt text from img tag
-                        .replace(/<\/?[a-z][^>]*>/g, ' ') // remove all tags
-                        .replace(/\s+/g, ' ')
-                        .trim(); // cleanup whitespace
-                    tweets.push(tweet_text);
+            // Set a timeout to run after scrolling ends
+            isScrolling = setTimeout(callback, refresh);
+        },
+        false
+    );
+};
+
+let calculate_sentiment = async () => {
+    try {
+        let tweets = extract_content_xpath(
+            '//article[@data-testid="tweet"]//div[@data-testid="tweetText"]',
+            'tweet_text'
+        );
+
+        let user_tweets = extract_content_xpath(
+            '//article[@data-testid="tweet"]//div[@data-testid="User-Names"]',
+            'user_tweet_text'
+        );
+
+        let ld_data = await language_detection_endpoint(tweets);
+
+        if (ld_data.length > 0) {
+            let english_tweet_index = [];
+            let filter_en_data = [];
+
+            await ld_data.map((tweet) => {
+                if (tweet.is_english) {
+                    english_tweet_index.push(1);
+                    filter_en_data.push({
+                        tweet_text: tweet.tweet_text
+                    });
+                } else {
+                    english_tweet_index.push(0);
                 }
+            });
 
-                while ((node = user_results.iterateNext())) {
-                    let tweet_text = node.lastChild.children[0];
-                    user_tweets.push(tweet_text);
-                }
+            let ss_data = await sentiment_score_endpoint(filter_en_data);
 
-                let prev_tweets = await chrome.storage.sync.get(['tweets']);
-                let prev_user_tweets = await chrome.storage.sync.get([
-                    'user_tweets'
-                ]);
+            let moods = ss_data.map((mood) => mood.detected_mood);
 
-                let mood_emoji = (mood) => {
-                    if (mood == 'POSITIVE') {
-                        return String.fromCodePoint(128522);
-                    } else if (mood == 'NEUTRAL') {
-                        return String.fromCodePoint(128528);
-                    } else if (mood == 'NEGATIVE') {
-                        return String.fromCodePoint(128577);
-                    } else {
-                        return 'NO_MOOD';
-                    }
-                };
+            let final_moods = generate_moods(english_tweet_index, moods);
 
-                let generate_moods = (indexes, moods) => {
-                    output = [];
-                    mood_index = 0;
-                    for (let i = 0; i < indexes.length; i++) {
-                        if (indexes[i] == 1) {
-                            output.push(mood_emoji(moods[mood_index]));
-                            mood_index += 1;
-                        } else {
-                            output.push('NO_MOOD');
-                        }
-                    }
-
-                    return output;
-                };
-
-                if (
-                    prev_tweets.tweets &&
-                    prev_user_tweets &&
-                    JSON.stringify(tweets) !==
-                        JSON.stringify(prev_tweets.tweets)
-                ) {
-                    try {
-                        await chrome.storage.sync.set({ tweets: tweets });
-                        let ld_data = await language_detection_endpoint(tweets);
-
-                        if (ld_data.length > 0) {
-                            let english_tweet_index = [];
-                            let filter_en_data = [];
-
-                            await ld_data.map((tweet) => {
-                                if (tweet.is_english) {
-                                    english_tweet_index.push(1);
-                                    filter_en_data.push({
-                                        tweet_text: tweet.tweet_text
-                                    });
-                                } else {
-                                    english_tweet_index.push(0);
-                                }
-                            });
-
-                            let ss_data = await sentiment_score_endpoint(
-                                filter_en_data
-                            );
-
-                            let moods = ss_data.map(
-                                (mood) => mood.detected_mood
-                            );
-
-                            let final_moods = generate_moods(
-                                english_tweet_index,
-                                moods
-                            );
-
-                            if (final_moods) {
-                                for (let i = 0; i < final_moods.length; i++) {
-                                    let mood_html = `
-                                    <div dir="ltr" aria-hidden="true" class="css-901oao r-14j79pv r-1q142lx r-37j5jr r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-s1qlax r-qvutc0"><span class="css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0">·</span></div>
-                                    <div class="tweet_mood"><p style="font-size: 15px;color: rgb(83, 100, 113);font-family: TwitterChirp, -apple-system, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, Helvetica, Arial, sans-serif;">Detected Mood: ${final_moods[i]}</p></div>
-                                    `;
-                                    if (
-                                        user_tweets[i] &&
-                                        user_tweets[i].lastChild &&
-                                        user_tweets[i].lastChild.classList &&
-                                        user_tweets[i].lastChild.classList[0] !=
-                                            'tweet_mood' &&
-                                        final_moods[i] != 'NO_MOOD'
-                                    ) {
-                                        user_tweets[i]?.insertAdjacentHTML(
-                                            'beforeend',
-                                            mood_html
-                                        );
-                                    }
-                                }
-                            }
-                            // console.log('Mood Data : ', final_moods);
-                        }
-                    } catch (e) {
-                        console.log('Error : ', e);
+            if (final_moods) {
+                for (let i = 0; i < final_moods.length; i++) {
+                    let mood_html = `
+                    <div dir="ltr" aria-hidden="true" class="css-901oao r-14j79pv r-1q142lx r-37j5jr r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-s1qlax r-qvutc0"><span class="css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0">·</span></div>
+                    <div class="tweet_mood"><p style="font-size: 15px;color: rgb(83, 100, 113);font-family: TwitterChirp, -apple-system, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, Helvetica, Arial, sans-serif;">Detected Mood: ${final_moods[i]}</p></div>
+                    `;
+                    if (
+                        user_tweets[i] &&
+                        user_tweets[i].lastChild &&
+                        user_tweets[i].lastChild.classList &&
+                        user_tweets[i].lastChild.classList[0] != 'tweet_mood' &&
+                        final_moods[i] != 'NO_MOOD'
+                    ) {
+                        user_tweets[i]?.insertAdjacentHTML(
+                            'beforeend',
+                            mood_html
+                        );
                     }
                 }
             }
         }
-    };
+    } catch (e) {
+        console.log('Error : ', e);
+    }
+};
 
-    // Create an observer instance linked to the callback function
-    const observer = new MutationObserver(callback);
+waitForElm('section').then(async (elm) => {
+    waitForElm('article').then(async (elment) => {
+        await calculate_sentiment();
+    });
 
-    // Start observing the target node for configured mutations
-    observer.observe(targetNode, config);
+    scrollStop(async () => {
+        await calculate_sentiment();
+    });
 });
